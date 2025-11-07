aaf64f91bc5a13756faa4c8867eeef157cc867a1
END1502 <86548217+END1502@users.noreply.github.com>
2025-11-07 14:26:57 +0800

    feat: Add event listener to submitBtn

--- main.js
+++ main.js
@@ -2,4 +2,10 @@ const list = document.querySelector("ul");
 
 const itm = document.querySelector("input");
 
-const btn = document.querySelector("button");
+const submitBtn = document.querySelector("button");
+
+submitBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+
+})
+
