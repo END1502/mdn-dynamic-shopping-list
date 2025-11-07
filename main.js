fe0409de5d230e97362aa23562a258d372647277
END1502 <86548217+END1502@users.noreply.github.com>
2025-11-07 14:27:42 +0800

    feat: add element for list item

--- main.js
+++ main.js
@@ -6,6 +6,10 @@ const submitBtn = document.querySelector("button");
 
 submitBtn.addEventListener('click', (event) => {
     event.preventDefault();
+    
+    const listItem = document.createElement("li");
+    const span = document.createElement("span");
+    const delBtn = document.createElement("button");
 
 })
 
