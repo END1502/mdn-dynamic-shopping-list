979230159316fb2b9eb6f5e4f578536c6c9f5177
END1502 <86548217+END1502@users.noreply.github.com>
2025-11-07 14:37:57 +0800

    feat: Get input value then reset

--- main.js
+++ main.js
@@ -6,7 +6,10 @@ const submitBtn = document.querySelector("button");
 
 submitBtn.addEventListener('click', (event) => {
     event.preventDefault();
-    
+
+    const listElement = itm.value;
+    itm.value = "";
+
     const listItem = document.createElement("li");
     const span = document.createElement("span");
     const delBtn = document.createElement("button");
